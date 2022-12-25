e70decd540e70538eb389f26826157dd20ba66a5
wakhoo <qqww212@korea.ac.kr>
2022-12-25 19:12:45 +0900

    hard

---
+++ src/components/SmallProgress.tsx
@@ -0,0 +1,151 @@
+import React,{useEffect, useRef} from "react";
+import {Animated, StyleSheet, Text,View} from "react-native"
+interface MData{
+    totalMin:number;
+    todayMin:number;
+    week:string;
+}
+
+function SmallProgressBar({totalMin,todayMin,week}:MData){
+    const loaderValue = useRef(new Animated.Value(0)).current;
+
+    const load = (count: number)=>{
+        Animated.timing(loaderValue,{
+            toValue:(count/totalMin)*100,
+            duration:500,
+            useNativeDriver:false
+        }).start();
+    }
+
+    const width = loaderValue.interpolate({
+        inputRange:[0,100],
+        outputRange:["0%","100%"],
+        extrapolate:"clamp"
+    });
+
+    useEffect(()=>{
+        load(todayMin)
+    },[todayMin]);
+    const dayOfWeek='목'
+    const Date=()=>{
+        if(dayOfWeek==week){
+            return(
+                <View>
+               
+                <Sucess/> 
+                <View style={styles.red}/>
+                </View>
+            )
+        }
+        else{
+            return(
+                <Sucess/>
+            )
+        }
+    }
+   const Sucess=()=>{
+    if(totalMin==todayMin){
+        return(
+            <View>
+                    <View style={styles.bar2}>
+                        
+                        <Animated.View
+                        style={{
+                            backgroundColor:"#61E294",
+                            width,
+                            height:33,
+                            borderRadius:5,
+                        }}
+                        ><Text style={styles.date}>{week}</Text></Animated.View>
+                    </View>
+                    {/* <Text style={styles.step}>{todayMin}/{totalMin}</Text> */}
+                </View>
+        )
+    }
+    else{
+        return(
+            <View>
+            <View style={styles.bar}>
+                
+                <Animated.View
+                style={{
+                    backgroundColor:"#B0F0C9",
+                    width,
+                    height:33,
+                    borderTopLeftRadius:5,
+                    borderBottomLeftRadius:5
+                    
+                }}
+                ><Text style={styles.date}>{week}</Text></Animated.View>
+            </View>
+            {/* <Text style={styles.step}>{todayMin}/{totalMin}</Text> */}
+        </View>
+        )
+    }
+   } 
+return(
+   <Date/>
+)
+}
+
+
+
+const styles=StyleSheet.create({
+    bar:{
+        
+        width:35,
+        height:35,
+        transform:[{rotate:'270deg'}],
+        backgroundColor:"#fff",
+        borderColor:"#bbb",
+        borderWidth:1,
+        borderRadius:5,
+        marginTop:-10,
+        // top:20,
+        left:10,
+        marginRight:15,
+        alignContent:'center',
+        justifyContent:'center'
+        
+    },
+    bar2:{
+        
+        width:35,
+        height:35,
+        transform:[{rotate:'270deg'}],
+        backgroundColor:"#fff",
+        borderColor:"#61E294",
+        borderWidth:1,
+        borderRadius:5,
+        marginTop:-10,
+        // top:20,
+        left:10,
+        marginRight:15,
+        alignContent:'center',
+        justifyContent:'center'
+        
+    },
+    step:{
+        color:"#000",
+
+    },
+    date:{
+        transform:[{rotate:'-270deg'}],
+        position:'absolute',
+        top:9,
+        left:10,
+        fontWeight:'bold'
+        
+    },
+    red:{
+        borderRadius:100,
+        borderWidth:5,
+        position: 'absolute',
+        borderColor:'red',
+        // top:35,
+        top:-12,
+        left:38
+    }
+})
+
+export default SmallProgressBar;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import { Animated, Dimensions, StyleSheet, Text, TouchableOpacity,View } from 'r
 
 import CircleProgressBar from '@app/components/CircleProgress'
 import ProgressBar from '@app/components/ProgressBar'
+import SmallProgressBar from '@app/components/SmallProgress'
 import { STATUS, SwipeablePanel } from '@app/components/SwipeablePanel'
 const HomeScreen: React.FC = () => {
 	const [panelStatus, setPanelStatus] = useState(2)
@@ -59,27 +60,25 @@ const HomeScreen: React.FC = () => {
 		todayMinutes: 6,
 		currentDays: 5,
 	}
-
-
-	return (
-		
-		<View
-			style={css`
-				flex: 1;
-				background: #000; 
-			`}
-		>
-			<SwipeablePanel {...panelProps} isActive={isPanelActive}>
-                    <View style={[css`
-                        padding: 24px;
-                        padding-bottom: 50px;
-                        padding-top: 30px;
-                        flex-direction: column;
-                    `, { height: panelHeight  }]}>
-                        <Text style={css`font-size: 18px; text-align: center;`}>이번 주에 4일 연속으로 접속하셨어요.</Text>
-                        {/* target이랑 today랑 차이가 나면 뜨게 함 */}
-                        <Text style={css`font-size: 18px; text-align: center`}>{userData.targetMinutes-data.todayMinutes}분 더 대화하시면 오늘 목표가 달성돼요!</Text>
-                        <View style={[css`
+    const Goal=()=>{
+        if(data.todayMinutes!=userData.targetMinutes){
+            return(
+                <Text style={css`font-size: 18px; text-align: center`}>{userData.targetMinutes-data.todayMinutes}분 더 대화하시면 오늘 목표가 달성돼요!</Text>
+            )
+        }
+        else{
+            return(
+                <Text style={css`font-size: 18px; text-align: center`}>오늘의 목표를 달성하였습니다!</Text>
+            )
+        }
+    }
+    const Progress=()=>{
+        if(panelStatus==2){
+            return(
+                <View>
+                    <Text style={css`font-size: 18px; text-align: center;`}>이번 주에 4일 연속으로 접속하셨어요.</Text>
+                    <Goal/>
+                     <View style={[css`
                             flex: 0.3;
                             flex-direction: row;
                             justify-content: flex-end;
@@ -115,6 +114,59 @@ const HomeScreen: React.FC = () => {
                         <View style={styles.btn}>
                         <TouchableOpacity><Text style={{fontWeight:'bold',textAlign:'center',fontSize:20}}>시작하기</Text></TouchableOpacity>
                         </View>
+                        </View>
+            )
+        }
+        else{
+            return(
+                <View>
+                    
+                     <View style={[css`
+                            flex: 0.3;
+                            flex-direction: row;
+                            justify-content: flex-end;
+                            // background-color: red;
+                            // height:"100%";
+                        `]}>
+                            
+                            
+                            {/* <MBText>{JSON.stringify(greetings.data)}</MBText> */}
+                            {/* Enter statistics */}
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={20} week={'월'}/>
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={0} week={'화'}/>
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={20} week={'수'}/>
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={data.todayMinutes} week={'목'}/>
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={0} week={'금'}/>
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={0} week={'토'}/>
+                            <SmallProgressBar totalMin={userData.targetMinutes} todayMin={0} week={'일'}/>
+                            
+				        </View> 
+
+                        
+                        <TouchableOpacity style={styles.btn2}><Text style={{fontWeight:'bold',textAlign:'center',fontSize:20}}>시작하기</Text></TouchableOpacity>
+                        
+			
+		</View>
+            )
+        }
+    }
+
+	return (
+		
+		<View
+			style={css`
+				flex: 1;
+				background: #000; 
+			`}
+		>
+			<SwipeablePanel {...panelProps} isActive={isPanelActive}>
+                    <View style={[css`
+                        padding: 24px;
+                        padding-bottom: 50px;
+                        padding-top: 30px;
+                        flex-direction: column;
+                    `, { height: panelHeight  }]}>
+                        <Progress/>
                     </View>
                     
                    
@@ -148,13 +200,26 @@ const styles=StyleSheet.create({
         borderRadius:8
     },
     circle:{
-        marginTop:100,
-        marginBottom:40,
+        marginTop:170,
+        marginBottom:30,
         flexDirection:'row',
         justifyContent:'space-around'
     },
     perCircle:{
         height:95,
         justifyContent:"space-between"
+    },
+    btn2:{
+        position:'absolute',
+        top:60,
+        // left:"50%",
+        width:'100%',
+        backgroundColor:'#61E294',
+        // width:"100%",
+        height:50,
+        justifyContent:"center",
+        alignContent:'center',
+        borderRadius:8
+
     }
 })
